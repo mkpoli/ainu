82e5da0d65d2e1f3980b80e41a19d9b6f555a7bb
mkpoli <git@mkpo.li>
2023-09-12 22:04:39 +0900

    Add basic conversion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,42 @@
-<svelte:head>
-	<title>Aynuitak Inuypa Ukosammaani Sayto</title>
-</svelte:head>
+<script lang="ts">
+	import { convertLatn2Kana } from '$lib/convert';
+	let script: 'Latn' | 'Kana' | 'Cyrl' = 'Latn';
 
-<h1>Aynuitak Inuypa Ukosammaani Sayto</h1>
+	$: t = (text: string): string => {
+		if (script === 'Latn') {
+			return text;
+		} else if (script === 'Kana') {
+			return convertLatn2Kana(text);
+			// return text.replace(/([ガ-ヶー])/, '$1ㇰ');
+		} else if (script === 'Cyrl') {
+			// return text.replace(/([А-Яа-яёЁ])/, '$1ӈ');
+			return text;
+		} else {
+			throw new Error('Unknown script');
+		}
+	};
+</script>
+
+<svelte:head>
+	<title>{t('Aynuitak Inuypa Ukosammaani Sayto')}</title>
+</svelte:head>
+<label for="script-select">{t('tokpa-inuye')}</label>
+<select id="script-select" name="script" bind:value={script}>
+	<option value="Latn">rómaunkur-itakitokpa</option>
+	<option value="Kana">カタカナ イタキトㇰパ</option>
+	<option value="Cyrl" disabled>Нуча-итакитокпа</option>
+</select>
+
+<h1>{t('Aynuitak Inuypa Ukosammaani Sayto')}</h1>
 <div role="doc-subtitle" lang="ja">アイヌ語執筆支援サイト</div>
 <div role="doc-subtitle" lang="en">Ainu Language Writing Support Site</div>
 <div role="doc-subtitle" lang="ru">Сайт поддержки письма на языке айну</div>
 
 <style>
+	select {
+		margin: 0 auto;
+	}
+
 	h1 {
 		font-size: 2em;
 		font-weight: bold;
