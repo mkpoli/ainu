3d9495967c515d859338a35f3c0623646ae3aa17
mkpoli <git@mkpo.li>
2023-09-12 20:53:02 +0900

    Add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Aynuitak Inuypa Ukosammaani Sayto</title>
+</svelte:head>
+
+<h1>Aynuitak Inuypa Ukosammaani Sayto</h1>
+<div role="doc-subtitle" lang="ja">アイヌ語執筆支援サイト</div>
+<div role="doc-subtitle" lang="en">Ainu Language Writing Support Site</div>
+<div role="doc-subtitle" lang="ru">Сайт поддержки письма на языке айну</div>
+
+<style>
+	h1 {
+		font-size: 2em;
+		font-weight: bold;
+		margin: 0;
+		text-align: center;
+	}
+	div[role='doc-subtitle'] {
+		font-size: 1.5em;
+		font-weight: bold;
+		margin: 0;
+		text-align: center;
+	}
+</style>
