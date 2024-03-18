6e4ab9b08b6bcaf9d46fa8b1c1e5972c5e5bbdcf
mkpoli <git@mkpo.li>
2024-03-18 18:27:46 +0900

    Use dynamic population

--- src/lib/DictionaryTable.svelte
+++ src/lib/DictionaryTable.svelte
@@ -1,5 +1,61 @@
 <script lang="ts">
 	import { t } from '$lib/convert';
+
+	type Dictionary = {
+		url: string;
+		name: string;
+		languages: string[];
+		count: number;
+	};
+
+	const dictionaries: Dictionary[] = [
+		{
+			url: 'https://ainugo.nam.go.jp/dic',
+			name: '国立アイヌ民族博物館アイヌ語アーカイブ',
+			languages: ['Aynuitak-Sisamitak', 'アイヌ語・日本語'],
+			count: 27000
+		},
+		{
+			url: 'http://hdl.handle.net/2115/87707',
+			name: '和愛辞典 : 草稿版 （太田満 編）',
+			languages: ['Sisamitak-Aynuitak', '日本語・アイヌ語'],
+			count: 12500
+		},
+		{
+			url: 'https://ainu.ninjal.ac.jp/topic/',
+			name: 'アトピック別 アイヌ語会話辞典',
+			languages: ['Sisamitak-Aynuitak', '日本語・アイヌ語'],
+			count: 3500
+		},
+		{
+			url: 'https://ainu.ninjal.ac.jp/topic/en/',
+			name: 'Topical Dictionary of Conversational Ainu',
+			languages: ['Inkiriskuritak-Aynuitak', 'English-Ainu'],
+			count: 3500
+		},
+		{
+			url: 'https://ja.wiktionary.org/wiki/%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA:%E3%82%A2%E3%82%A4%E3%83%8C%E8%AA%9E',
+			name: '日本語版Wiktionary',
+			languages: ['Aynuitak-Sisamitak', 'アイヌ語・日本語'],
+			count: 2000
+		},
+		{
+			url: 'https://itak.aynu.org/',
+			name: 'Tane an Aynuitak-kotupte Itak-uwoeroskip\n現代アイヌ語翻訳用語集\nModern Ainu Translation Glossary',
+			languages: [
+				'Aynuitak-Sisamitak-Inkiriskuritak',
+				'アイヌ語・日本語・英語',
+				'Ainu-Japanese-English'
+			],
+			count: 1000
+		},
+		{
+			url: 'https://en.wiktionary.org/wiki/Category:Ainu_language',
+			name: 'English Wiktionary',
+			languages: ['Aynuitak-Inkiriskuritak', 'Ainu-English'],
+			count: 600
+		}
+	];
 </script>
 
 <table>
@@ -11,91 +67,19 @@
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
-			<td>
-				<a href="https://ainugo.nam.go.jp/dic" lang="ja" target="_blank">
-					国立アイヌ民族博物館アイヌ語アーカイブ
-				</a>
-			</td>
-			<td>
-				<span>Aynuitak-Sisamitak</span><br /><span lang="ja">アイヌ語・日本語</span>
-			</td>
-			<td> 27000 </td>
-		</tr>
-		<tr>
-			<td>
-				<a href="http://hdl.handle.net/2115/87707" lang="ja" target="_blank">
-					和愛辞典 : 草稿版 （太田満 編）</a
-				>
-			</td>
-			<td>
-				<span>Sisamitak-Aynuitak</span><br /><span lang="ja">日本語・アイヌ語</span>
-			</td>
-			<td> 12500 </td>
-		</tr>
-		<tr>
-			<td>
-				<a href="https://ainu.ninjal.ac.jp/topic/" lang="ja" target="_blank">
-					アトピック別 アイヌ語会話辞典</a
-				>
-			</td>
-			<td>
-				<span>Sisamitak-Aynuitak</span><br /><span lang="ja">日本語・アイヌ語</span>
-			</td>
-			<td> 3500 </td>
-		</tr>
-		<tr>
-			<td>
-				<a href="https://ainu.ninjal.ac.jp/topic/en/">Topical Dictionary of Conversational Ainu</a>
-			</td>
-			<td>
-				<span>Inkiriskuritak-Aynuitak</span><br /><span lang="en">English-Ainu</span>
-			</td>
-			<td> 3500 </td>
-		</tr>
-		<tr>
-			<td>
-				<a
-					href="https://ja.wiktionary.org/wiki/%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA:%E3%82%A2%E3%82%A4%E3%83%8C%E8%AA%9E"
-					lang="ja"
-					target="_blank"
-				>
-					日本語版Wiktionary
-				</a>
-			</td>
-			<td>
-				<span>Aynuitak-Sisamitak</span><br /><span>アイヌ語・日本語</span>
-			</td>
-			<td> 2000 </td>
-		</tr>
-		<tr>
-			<td>
-				<a href="https://itak.aynu.org/" target="_blank">
-					<span lang="ja">現代アイヌ語翻訳用語集</span>
-					<br />
-					<span lang="en">Modern Ainu Translation Glossary</span>
-				</a>
-			</td>
-			<td>
-				<span>Aynuitak-Inkiriskuritak-Sisamitak</span>
-				<br />
-				<span lang="en">Ainu-English-Japanese</span>
-				<br />
-				<span lang="ja">アイヌ語・英語・日本語</span>
-			</td>
-			<td> 1000 </td>
-		</tr>
-		<tr>
-			<td>
-				<a href="https://en.wiktionary.org/wiki/Category:Ainu_language" lang="en" target="_blank">
-					English Wiktionary
-				</a>
-			</td>
-			<td>
-				<span>Aynuitak-Inkiriskuritak</span><br /><span lang="en">Ainu-English</span>
-			</td>
-			<td> 600 </td>
-		</tr>
+		{#each dictionaries as { url, name, languages, count }}
+			<tr>
+				<td>
+					<a href={url} lang="ja" target="_blank">{@html name.replaceAll(/\n/g, '<br />')}</a>
+				</td>
+				<td>
+					{#each languages as language}
+						<span>{language}</span><br />
+					{/each}
+				</td>
+				<td> {count} </td>
+			</tr>
+		{/each}
 	</tbody>
 </table>
 
