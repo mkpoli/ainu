9dafab545d2bef845a730113f297d813b4d494ab
mkpoli <git@mkpo.li>
2024-11-06 23:22:36 +0900

    Fix ScriptSelect styling

--- src/lib/ScriptSelect.svelte
+++ src/lib/ScriptSelect.svelte
@@ -3,35 +3,19 @@
 	import TablerLanguageKatakana from '~icons/tabler/language-katakana';
 </script>
 
-<div class="script-select">
-	<label for="script-select">
+<div class="flex items-center justify-end gap-4">
+	<label class="flex items-center justify-right gap-2" for="script-select">
 		<TablerLanguageKatakana />
 		{$t('Itokpa-Inuye')}</label
 	>
-	<select id="script-select" name="script" bind:value={$script}>
+	<select
+		id="script-select"
+		name="script"
+		bind:value={$script}
+		class="bg-white text-slate-950 text-sm px-2 w-48 leading-tight rounded-md"
+	>
 		<option value="Latn">Rómaunkur-Itakitokpa</option>
 		<option value="Kana">カタカナ イタキトㇰパ</option>
 		<option value="Cyrl">Нуча-итакитокпа</option>
 	</select>
 </div>
-
-<style>
-	.script-select {
-		display: flex;
-		align-items: center;
-		justify-content: right;
-		gap: 1em;
-	}
-
-	label {
-		display: flex;
-		align-items: center;
-		justify-content: right;
-		gap: 0.5em;
-	}
-
-	select {
-		padding: 0.5em;
-		line-height: 1.5em;
-	}
-</style>
