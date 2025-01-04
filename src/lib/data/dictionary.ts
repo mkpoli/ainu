eb305d43ca1835981b8798535cd02295ad6a8aac
mkpoli <git@mkpo.li>
2025-01-04 16:40:46 +0900

    feat: reduce duplicated dictionary

--- src/lib/data/dictionary.ts
+++ src/lib/data/dictionary.ts
@@ -14,16 +14,27 @@ export const PERSONAL_AFFIXES = [
 	'=an',
 	'=as'
 ];
-
-export const dictionary = [
-	...WORD_FREQ_LIST,
-	...PERSONAL_AFFIXES.map((word) => ({ word, freq: 9999 })),
-	...EXTRA_WORD_LIST.split('\n')
-		.filter(
-			(word) =>
-				word &&
-				!PERSONAL_AFFIXES.includes(word) &&
-				!WORD_FREQ_LIST.find(({ word: w }) => w === word)
+export const dictionary = Object.values(
+	[...WORD_FREQ_LIST, ...PERSONAL_AFFIXES.map((word) => ({ word, freq: 9999 }))]
+		.concat(
+			EXTRA_WORD_LIST.split('\n')
+				.filter(
+					(word) =>
+						word &&
+						!PERSONAL_AFFIXES.includes(word) &&
+						!WORD_FREQ_LIST.find(({ word: w }) => w === word)
+				)
+				.map((word) => ({ word, freq: 0 }))
 		)
-		.map((word) => ({ word, freq: 0 }))
-];
+		.reduce(
+			(acc, { word, freq }) => {
+				if (!acc[word]) {
+					acc[word] = { word, freq };
+				} else {
+					acc[word].freq += freq;
+				}
+				return acc;
+			},
+			{} as Record<string, { word: string; freq: number }>
+		)
+);
